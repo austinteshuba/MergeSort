a2778478136148c48b9a0e78af3a786fd9fd872a
austinteshuba <austinteshuba@gmail.com>
2018-08-15 11:13:22 -0400

    MergeSort.py

---
+++ MergeSort.py
@@ -0,0 +1,50 @@
+from random import *
+#inp=open("text.txt", "r").readlines()
+inp=[9, 12, 3, 1, 6, 8, 2, 5, 14, 13, 11, 7, 10, 4, 0]
+#numbers = [x for x in range(0, 1000000)]
+#numbers.reverse()
+numbers=[6,5,4,3,2,1]
+def inputing(path):
+    inp = [int(x) for x in open(path, "r").readlines()]
+    def mergeSort(array,inv):
+        if len(array)<=1:
+            return 0
+        else:
+            midpoint = len(array)//2
+            leftSide = array[:midpoint]
+            rightSide= array[midpoint:]
+            
+            invL=mergeSort(leftSide,0)
+            invR=mergeSort(rightSide,0)
+            leftIndex=0
+            rightIndex=0
+            k=0
+            inv+=invL+invR
+            while leftIndex<len(leftSide) and rightIndex<len(rightSide):
+                if leftSide[leftIndex]<=rightSide[rightIndex]:#change the operator here to make the sort increasing or decreasing
+                    array[k]=leftSide[leftIndex]
+                    leftIndex+=1
+                    k+=1
+                else:#handles equality cases
+                    array[k]=rightSide[rightIndex]
+                    rightIndex+=1
+                    k+=1
+                    inv+=len(leftSide)-leftIndex
+                    #print(len(leftSide)-leftIndex, inv)
+            while leftIndex < len(leftSide):
+                array[k]=leftSide[leftIndex]
+                leftIndex+=1
+                k+=1
+                
+            while rightIndex < len(rightSide):
+                array[k]=rightSide[rightIndex]
+                rightIndex+=1
+                k+=1
+                inv+=len(leftSide)-leftIndex
+            #print(inv)
+            return inv
+    return mergeSort(inp, 0)
+print(inputing("text.txt"))
+print("Done!")
+
+            
